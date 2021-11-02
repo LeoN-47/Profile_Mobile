219ae9032ae88d89822b8f1851da92127edf1004
Fahri_Asnawian <fahriasnawian5@gmail.com>
2021-11-02 21:30:42 +0700

    tugas android

---
+++ app/src/main/java/com/leon/tugas_mobile_andorid_ii/MainActivity.java
@@ -0,0 +1,14 @@
+package com.leon.tugas_mobile_andorid_ii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
